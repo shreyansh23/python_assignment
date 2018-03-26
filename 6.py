c3d31ac44bff3dc83699f7ac526138a10919df45
Shreyansh Jain <jain.shreyansh0@gmail.com>
2018-03-26 14:14:52 +0530

    Correction made in 6.py

--- 6.py
+++ 6.py
@@ -2,7 +2,11 @@ n=int(input("enter no of terms: "))
 fib=[]
 fib.append(0)
 fib.append(1)
-list = [fib[k-1]+fib[k-2] for k in range(2,n)]
+for k in range(2,n):
+    fib.append(fib[k-1]+fib[k-2])
+
+
+
 #fib += list
-#print(fib)
+print(fib)
 #print(list(map(lambda x : x**3 , fib )))
